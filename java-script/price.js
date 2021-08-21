a968577fa9d21324843ca4e8e210f1ae06b42568
Minhajul-Shobuj <mdminhajulislamshobuj@gmail.com>
2021-08-21 16:54:11 +0600

    js-update

--- java-script/price.js
+++ java-script/price.js
@@ -1,54 +1,51 @@
 //function for declare all coasts;
-function allcoasts(id, value) {
+function productCoast(id, value) {
     const price = document.getElementById(id);
     price.innerText = value;
+
+    totalprice()
 }
 
 //for memory
 document.getElementById('8GB-memory-btn').addEventListener('click', function () {
-    allcoasts('extra-memory-coast', 0);
-
+    productCoast('extra-memory-coast', 0);
 })
 document.getElementById('16GB-memory-btn').addEventListener('click', function () {
-    allcoasts('extra-memory-coast', 180);
+    productCoast('extra-memory-coast', 180);
 })
 
 // for Storage
 document.getElementById('256GB-storage-btn').addEventListener('click', function () {
-    allcoasts('extra-storage-coast', 0);
+    productCoast('extra-storage-coast', 0);
 })
 document.getElementById('512GB-storage-btn').addEventListener('click', function () {
-    allcoasts('extra-storage-coast', 100);
+    productCoast('extra-storage-coast', 100);
 })
 document.getElementById('1TB-storage-btn').addEventListener('click', function () {
-    allcoasts('extra-storage-coast', 180);
+    productCoast('extra-storage-coast', 180);
 })
 
 // for delivery 
 document.getElementById('free-delivery-btn').addEventListener('click', function () {
-    allcoasts('delivery-charge', 0);
+    productCoast('delivery-charge', 0);
 })
 document.getElementById('paid-delivery-btn').addEventListener('click', function () {
-    allcoasts('delivery-charge', 20);
+    productCoast('delivery-charge', 20);
 })
 
 // for total
-// function totalcalc() {
-//     var memoryPrice = document.getElementById('extra-memory-coast').innerText
-//     console.log(memoryPrice)
-//     var storagePrice = parseFloat(document.getElementById('extra-storage-coast').innerText)
-//     var deliveryCharge = parseFloat(document.getElementById('delivery-charge').innerText)
-//     var total = document.getElementById('total-coast');
-//     total.innerText = memoryPrice + storagePrice + deliveryCharge;
-// }
+function productPrice(id) {
+    const product = document.getElementById(id).innerText
+    return product;
+}
+function totalprice() {
+    const memory = productPrice('extra-memory-coast')
+    const storage = productPrice('extra-storage-coast')
+    const delivary = productPrice('delivery-charge')
+    const memoreyPrice = parseFloat(memory);
+    const storagePrice = parseFloat(storage);
+    const delivaryCoast = parseFloat(delivary);
+    const toatalCoast = memoreyPrice + storagePrice + delivaryCoast + 1299;
+    document.getElementById('total-coast').innerText = toatalCoast;
+}
 
-document.getElementById('total-coast').addEventListener("keydown", function (event) {
-
-})
-
-
-
-// totalPrice.innerText = 
-// document.getElementById('extra-memory-coast').innerText
-// console.log(totalPrice.innerText)
-//     + parseFloat(document.getElementById('extra-storage-coast').innerText) + parseFloat(document.getElementById('delivery-charge').innerText)
